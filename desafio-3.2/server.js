a33ae35d1498f54006d0ed466383a90f25c4b7a8
FehSchiavon <felipschiavon@gmail.com>
2020-05-07 17:11:36 -0300

    chore: Organizando as TempleEngine

--- desafio-3.2/server.js
+++ desafio-3.2/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
 server.get("/", function(req, res) {
-    return res.send("Hehe, deu certo!")
+    return res.render("about")
 })
+
+server.get("/courses", function(req, res) {
+    return res.render("courses")
+})
+
+// server.use(function(req, res) {
+//     return res.status(404).render("not-found");
+// })
 
 server.listen(7000, function(){
     console.log("server is runing")
