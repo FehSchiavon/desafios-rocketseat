40cbf9a184412d32dc7be254327a058f29f40d2b
FehSchiavon <felipschiavon@gmail.com>
2020-06-15 08:25:57 -0300

    Create Routes POST with REQ.BODY, but input validation

--- desafio-4.3/routes.js
+++ desafio-4.3/routes.js
@@ -9,8 +9,21 @@ routes.get('/register', function(req, res) {
     return res.render('teachers/register')
 })
 
-routes.post('/', function(req, res) {
-    return res.send('Recebido')
+routes.post('/', function(req, res) { //req.body e usado no method POST
+
+    const keys = Object.keys(req.body) // Object é Contrutor | E o .keys transforma em uma array, mas somente dos Objectos
+
+
+    // Validação se dos Dados estão Preenchidos
+    for (key of keys) { // Enviando todos os objetos para KEY
+
+        // req.body[elemento] | é igual o seletor | req.body.nome_do_elemento
+        if (req.body[key] == "")
+            return res.send('Please, fill all fields!')
+    }
+
+
+    return res.send(req.body)
 })
 
 module.exports = routes
