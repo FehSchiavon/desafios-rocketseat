a3ec1b9f47e799c17a4cccd4fdb03e47007e757b
FehSchiavon <felipschiavon@gmail.com>
2020-06-21 15:19:34 -0300

    Solver edit/date/formatation date/splice

--- desafio-4.4/utils.js
+++ desafio-4.4/utils.js
@@ -23,10 +23,10 @@ module.exports = {
     date: function(timestamp) {
         const date = new Date(timestamp)
 
-        const year = date.getUTCMonth()
+        const year = date.getUTCFullYear()
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         const day = `0${date.getUTCDate()}`.slice(-2)
 
-        return `${year}-${month}-${day}`
+        return `${day}-${month}-${year}`
     }
 }
