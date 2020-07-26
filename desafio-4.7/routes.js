8da1e57455a7e71c3138ae517f54c7ad5c9e6ac0
FehSchiavon <felipschiavon@gmail.com>
2020-07-26 17:30:44 -0300

    Create Routes Students

--- desafio-4.7/routes.js
+++ desafio-4.7/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const teachers = require('./teachers')
+const students = require('')
 
 routes.get('/', function(req, res) {
     return res.redirect('/login')
@@ -18,6 +19,10 @@ routes.put('/register', teachers.put)
 routes.delete('/register', teachers.delete)
 
 // Students
-
+routes.post('/register', students.post)
+routes.get('/register/:id', students.show)
+routes.get('/register/:id/edit', students.edit)
+routes.put('/register', students.put)
+routes.delete('/register', students.delete)
 
 module.exports = routes
