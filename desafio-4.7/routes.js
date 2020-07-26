986b560bc92b85c58faa102969a332eb5e92f8d3
FehSchiavon <felipschiavon@gmail.com>
2020-07-26 17:27:05 -0300

    Oganizarion routes

--- desafio-4.7/routes.js
+++ desafio-4.7/routes.js
@@ -6,27 +6,18 @@ routes.get('/', function(req, res) {
     return res.redirect('/login')
 })
 
-routes.get('/login', function(req, res) {
-    return res.render('teachers/login')
-})
-
-routes.get('/register', function(req, res) {
-    return res.render('teachers/register')
-})
-// LIST
-
-routes.get("/register/list", teachers.list)
-
+// Teachers
+routes.get('/login', teachers.indexLogin)
+routes.get('/register', teachers.indexRegister)
+routes.get("/register/list", teachers.list) // LIST
 // CRUD 
-
 routes.post('/register', teachers.post)
-
 routes.get('/register/:id', teachers.show)
-
 routes.get('/register/:id/edit', teachers.edit)
-
 routes.put('/register', teachers.put)
-
 routes.delete('/register', teachers.delete)
+
+// Students
+
 
 module.exports = routes
