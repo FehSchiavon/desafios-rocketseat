e2e4dcf5b37b794e488a5a62f1996ec287954597
FehSchiavon <felipschiavon@gmail.com>
2020-07-28 06:10:05 -0300

    Create data.js / students

--- desafio-4.7/controllers/students.js
+++ desafio-4.7/controllers/students.js
@@ -6,17 +6,17 @@ const { age, date } = require('../utils')
 // Index
 exports.index = function(req, res) {
     // Criar separação de lista
-    const listTearchers = data.teachers.map(function(teacher) {
-        const partTeacher = {
-            ...teacher,
-            plus: teacher.plus.split(',') // Devide o Obejto em um Array
+    const listTearchers = data.students.map(function(student) {
+        const partStudent = {
+            ...student,
+            plus: student.plus.split(',') // Devide o Obejto em um Array
         }
-        return partTeacher
+        return partStudent
     })
 
     console.log(listTearchers)
 
-    return res.render('teachers/list', { teachers: listTearchers})
+    return res.render('students/list', { students: listTearchers})
 }
 // Create
 exports.create = function(req, res) {
@@ -42,10 +42,10 @@ exports.post = function(req, res) { //req.body e usado no method POST
     
     birth = Date.parse(birth) // Construtors Manuais | Data em milisegundos
     const created_at = Date.now() // Construtor que pega data atual
-    const id = Number(data.teachers.length + 1) // Construtor para criar ID 
+    const id = Number(data.students.length + 1) // Construtor para criar ID 
 
     // Organizar a sequencia dos dados
-    data.teachers.push({
+    data.students.push({
         id,
         avatar_url,
         nickname,
@@ -71,69 +71,69 @@ exports.post = function(req, res) { //req.body e usado no method POST
 exports.show = function(req, res) { //req.params serve para coletar uma ID e USAR os DADOS dele
     const { id } = req.params
 
-    const foundTeacher = data.teachers.find(function(teacher) { //Encontrar o ID dentro do ARRAY
-        return id == teacher.id
+    const foundStudent = data.students.find(function(student) { //Encontrar o ID dentro do ARRAY
+        return id == student.id
     })
 
-    if(!foundTeacher) return res.send('Instructor not found!') // Caso ele não encontrar o ID
+    if(!foundStudent) return res.send('Instructor not found!') // Caso ele não encontrar o ID
 
-    const teacher = {
-        ...foundTeacher, // Manda todos os dados do Array que o formulario gerou
-        age: age(foundTeacher.birth), // Gera a idade conforme o Data de Nascimentos escolhida
-        // plus: foundTeacher.plus.split(","), // Separa os textos por virgula 
-        plus: foundTeacher.plus,
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at) // Formatar data yyyy-mm-dd
+    const student = {
+        ...foundStudent, // Manda todos os dados do Array que o formulario gerou
+        age: age(foundStudent.birth), // Gera a idade conforme o Data de Nascimentos escolhida
+        // plus: foundStudent.plus.split(","), // Separa os textos por virgula 
+        plus: foundStudent.plus,
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundStudent.created_at) // Formatar data yyyy-mm-dd
     }
 
-    // console.log(teacher) // Visualizar dados
+    // console.log(student) // Visualizar dados
 
-    // return res.send(foundTeacher) // Dados esta indo em formato JSON
-    return res.render('teachers/show', { teacher })
+    // return res.send(foundStudent) // Dados esta indo em formato JSON
+    return res.render('students/show', { student })
 
 }
 // Edit
 exports.edit = function(req, res) { //req.params do Data.json 
     const { id } = req.params // Colocar os dados dentro de um Objeto
 
-    const foundTeacher = data.teachers.find(function(instructor) { //Encontrar o ID dentro do ARRAY
+    const foundStudent = data.students.find(function(instructor) { //Encontrar o ID dentro do ARRAY
         return id == instructor.id
     })
 
-    if(!foundTeacher) return res.send('Teacher not found!') // Caso ele não encontrar o ID
+    if(!foundStudent) return res.send('Student not found!') // Caso ele não encontrar o ID
 
-    const teacher = {
-        ...foundTeacher, // Manda todos os dados do Array que o formulario gerou
-        birth: date(foundTeacher.birth) // Transforma os dados 2332321123 em data YYYY-MM-DD
+    const student = {
+        ...foundStudent, // Manda todos os dados do Array que o formulario gerou
+        birth: date(foundStudent.birth) // Transforma os dados 2332321123 em data YYYY-MM-DD
     }
 
-    // console.log(teacher) // Visualizar dados enviados no terminal
+    // console.log(student) // Visualizar dados enviados no terminal
 
-    return res.render('teachers/edit', { teacher })
+    return res.render('students/edit', { student })
 
 }
 // Update
 exports.put = function(req, res) { // Atualiza os dados do Array
     const { id } = req.body // Pega o id da Página
-    let index = 0 // Irá receber a resposta do foundTeacher
+    let index = 0 // Irá receber a resposta do foundStudent
 
-    const foundTeacher = data.teachers.find(function(teacher, foundIndex) { // Valida se existe esse index no Array
-        if (id == teacher.id) {
+    const foundStudent = data.students.find(function(student, foundIndex) { // Valida se existe esse index no Array
+        if (id == student.id) {
             index = foundIndex 
             return true // Caso existir volta o numero do index que é verdadeiro
         }
     })
 
-    if(!foundTeacher) return res.send('Teacher not found!') // Agora não encontrando ele avisa que não tem
+    if(!foundStudent) return res.send('Student not found!') // Agora não encontrando ele avisa que não tem
 
-    const teacher = {
-        ...foundTeacher,
+    const student = {
+        ...foundStudent,
         ...req.body,
         birth: Date.parse(req.body.birth), // Transforma em data normal YYYY-MM-DD
         id: Number(req.body.id) //Transformar STRING em NUMBER
     }
 
 
-    data.teachers[index] = teacher
+    data.students[index] = student
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
         if(err) return res.send('Write error!')
@@ -145,11 +145,11 @@ exports.put = function(req, res) { // Atualiza os dados do Array
 exports.delete = function(req, res) {
     const { id } = req.body
 
-    const filterTeachers = data.teachers.filter(function(teacher) {
-        return teacher.id != id
+    const filterstudents = data.students.filter(function(student) {
+        return student.id != id
     })
 
-    data.teachers = filterTeachers
+    data.students = filterstudents
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
         if (err) return res.send('Write file error!')
