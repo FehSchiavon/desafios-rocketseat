015578a2f0754cd4e4191bbb5470386b8890a3ad
FehSchiavon <felipschiavon@gmail.com>
2020-09-30 06:40:09 -0300

    edit / put

--- desafio-5.1/src/app/controllers/instructors.js
+++ desafio-5.1/src/app/controllers/instructors.js
@@ -43,50 +43,18 @@ exports.show = function(req, res) {
     return
 }
 exports.edit = function(req, res) {
-    // req.params
-    const { id } = req.params
-
-    const foundInstructor = data.instructors.find(function(instructor) {
-        return  id == instructor.id
-    })
-
-    if(!foundInstructor) return res.send('Instructor not found!')
-
-    const instructor = {
-        ...foundInstructor,
-        birth: date(foundInstructor.birth).iso
-    }
-
-    return res.render('instructors/edit', { instructor })
+    return
 }
 exports.put = function(req, res) {
-    // req.params
-    const { id } = req.body
-    let index = 0
+    const keys = Object.keys(req.body)
 
-    const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
-        if (id == instructor.id) {
-            index = foundIndex
-            return true
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields')
         }
-    })
-
-    if(!foundInstructor) return res.send('Instructor not found!')
-
-    const instructor = {
-        ...foundInstructor,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id)
     }
 
-    data.instructors[index] = instructor
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send('Write error!')
-
-        return res.redirect(`/instructors/${id}`)
-    })
+    return
 }
 exports.delete = function(req, res) {
     const { id } = req.body
