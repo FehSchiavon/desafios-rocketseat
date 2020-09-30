2ae7af1670a28c83e2bb1a0d6ddd4e13aab8ff04
FehSchiavon <felipschiavon@gmail.com>
2020-09-30 06:42:54 -0300

    integration all module exports instructors

--- desafio-5.1/src/app/controllers/instructors.js
+++ desafio-5.1/src/app/controllers/instructors.js
@@ -8,54 +8,36 @@ module.exports = {
         return res.render('instructors/create')
     },
     post(req, res) {
+        const keys = Object.keys(req.body)
 
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+        
+        let { avatar_url, birth, name, services, gender } = req.body
+    
+        return
     },
-    show(req, res) {},
-    edit(req, res) {},
-    put(req, res) {},
-    delete(req, res) {},
-}
+    show(req, res) {
+        return
+    },
+    edit(req, res) {
+        return
+    },
+    put(req, res) {
+        const keys = Object.keys(req.body)
 
-
-exports.index = function(req, res) {
-    return res.render('instructors/index', { instructors: data.instructors })
-}
-exports.create = function(req, res) {
-    return res.render('instructors/create')
-}
-exports.post = function(req, res) {
-
-    const keys = Object.keys(req.body)
-
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send('Please, fill all fields')
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
         }
-    }
     
-    let { avatar_url, birth, name, services, gender } = req.body
-
-    return
-    
-
-}
-exports.show = function(req, res) {
-    return
-}
-exports.edit = function(req, res) {
-    return
-}
-exports.put = function(req, res) {
-    const keys = Object.keys(req.body)
-
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send('Please, fill all fields')
-        }
-    }
-
-    return
-}
-exports.delete = function(req, res) {
-    return
+        return
+    },
+    delete(req, res) {
+        return
+    },
 }
