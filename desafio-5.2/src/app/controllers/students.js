7150c71fcc4d373eb2146b6952466ae89aa8244d
FehSchiavon <felipschiavon@gmail.com>
2020-10-20 06:36:23 -0300

    create delete controllers / students

--- desafio-5.2/src/app/controllers/students.js
+++ desafio-5.2/src/app/controllers/students.js
@@ -54,6 +54,8 @@ module.exports = {
         })
     },
     delete(req, res) {
-        return
+        Student.delete(req.body.id, function() {
+            return res.redirect(`/students`)
+        })
     },
 }
