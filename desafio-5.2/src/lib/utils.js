9d1c160a47a5dfd70fb8b9ed4033ea24a6230356
FehSchiavon <felipschiavon@gmail.com>
2020-11-03 06:45:59 -0300

    transformation is fail, but need to find

--- desafio-5.2/src/lib/utils.js
+++ desafio-5.2/src/lib/utils.js
@@ -21,6 +21,8 @@ module.exports = {
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         const day = `0${date.getUTCDate()}`.slice(-2)
 
+        console.log(`${day}/${month}/${year}`)
+
         return {
             day,
             month,
